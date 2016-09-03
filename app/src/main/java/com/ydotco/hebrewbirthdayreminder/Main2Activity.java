7bf71551eb94065e20463717ce1cf2a931c1e634
ydotco <ydotco@gmail.com>
2016-09-03 23:06:23 +0300

    added fab button and animations

--- app/src/main/java/com/ydotco/hebrewbirthdayreminder/Main2Activity.java
+++ app/src/main/java/com/ydotco/hebrewbirthdayreminder/Main2Activity.java
@@ -2,21 +2,27 @@ package com.ydotco.hebrewbirthdayreminder;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
     private RecyclerView recyclerView;
     private ContactAdapter contactAdapter;
     CalendarView calendarView;
+
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab, fab1, fab2;
+    private Animation fab_open, fab_close, rotate_forward, rotate_backward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,7 @@ public class Main2Activity extends AppCompatActivity {
         calendarView = (CalendarView) findViewById(R.id.calendarView);
         recyclerView = (RecyclerView) findViewById(R.id.rvCalendarContacts);
 
-        contactAdapter=new ContactAdapter(getApplication(),getContactList());
+        contactAdapter = new ContactAdapter(getApplication(), getContactList());
         recyclerView.setAdapter(contactAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplication()));
 
@@ -40,14 +46,17 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+
     }
 
     //setting up a list of contacts to be shown at the listview
@@ -76,4 +85,40 @@ public class Main2Activity extends AppCompatActivity {
     private void updateList(int year, int month, int dayOfMonth) {
     }
 
+    public void animateFAB() {
+
+        if (isFabOpen) {
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+        } else {
+
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.fab:
+                animateFAB();
+                break;
+            case R.id.fab1:
+                Toast.makeText(Main2Activity.this, "import manually", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fab2:
+                Toast.makeText(Main2Activity.this, "import from facebook", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
+
